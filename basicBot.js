4bf3da160fdf5793d8a5214590bbfd6b914b2021
fviii <twostepbehind@gmail.com>
2015-08-16 04:17:06 -0400

    Update basicBot.js
    Changed the !kick command permission to host only since BB isn't unbanning afterwords. Bouncers are using this to kick autojoiners who keep joining after BB removes them, and people are wondering what's up when they can't come back the following day.

--- basicBot.js
+++ basicBot.js
@@ -2360,7 +2360,7 @@
 
             kickCommand: {
                 command: 'kick',
-                rank: 'bouncer',
+                rank: 'host',
                 type: 'startsWith',
                 functionality: function (chat, cmd) {
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
